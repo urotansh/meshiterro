ac8854758dbd3225acc7e401e5df942006e1216c
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-10 16:11:46 +0000

    [Update] 4-9 post_imagesのルーティング設定をresourcesメソッドで設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'postimages/new'
-  get 'postimages/index'
-  get 'postimages/show'
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
+  
+  resources :post_images, only: [:new, :index, :show]
+  
   get 'homes/about', to: 'homes#about', as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
