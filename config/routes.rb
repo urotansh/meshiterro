3c877916c012bfc82aeb4781b5065212ab09c34b
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-11 04:55:35 +0000

    [Add] 4-10 投稿画面・投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
   root to: 'homes#top'
   devise_for :users
   
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   
   get 'homes/about', to: 'homes#about', as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
