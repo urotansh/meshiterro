faad3ab3c11948fcae02e753268655e3103588fd
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-12 04:18:41 +0000

    [Add] 4-18 コメント削除機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create]
+    resources :post_comments, only: [:create, :destroy]
   end
   
   resources :users, only: [:show, :edit, :update]
