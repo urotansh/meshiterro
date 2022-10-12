ea4ca099f088028c288ab95fbfcc28517e738866
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-12 06:05:56 +0000

    [Add] 4-19 いいね機能のアソシエーションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   
   resources :users, only: [:show, :edit, :update]
