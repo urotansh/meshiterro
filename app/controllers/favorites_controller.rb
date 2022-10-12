850b7ea5359ef1850f6198b80e406f8e89ab4b01
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-12 06:11:51 +0000

    [Add] 4-19 いいね機能の作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,21 @@
 class FavoritesController < ApplicationController
+  
+  def create
+    post_image = PostImage.find(params[:post_image_id])
+    #favorite = Favorite.new(post_image_id: post_image.id)
+    #favorite.user_id = current_user.id
+    favorite = current_user.favorites.new(post_image_id: post_image.id)
+    favorite.save
+    redirect_to post_image_path(post_image.id)
+  end
+  
+  def destroy
+    post_image = PostImage.find(params[:post_image_id])
+    #favorite = Favorite.find_by(post_image_id: post_image.id)
+    #favorite.user_id = current_user.id
+    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    favorite.destroy
+    redirect_to post_image_path(post_image.id)
+  end
+    
 end
