27f053beff6a886978ca4bae65d14a07711128ab
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-13 08:24:23 +0000

    [Add] 4-25 ユーザ編集/更新機能における他ユーザのアクセス制限(投稿一覧へリダイレクト)を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to post_images_path
+    end
+    
     @user = User.find(params[:id])
   end
   
   def update
+    
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to post_images_path
+    end
+    
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
