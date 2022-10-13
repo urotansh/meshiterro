521b304e49f3fa0e534e1b60cda381f4346e993d
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-13 08:43:48 +0000

    [Update] 4-25 ユーザ編集/更新機能におけるアクセス制限処理をbefore_actionで実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,16 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    is_matching_login_user
-    
     @user = User.find(params[:id])
   end
   
   def update
-    is_matching_login_user
-    
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
