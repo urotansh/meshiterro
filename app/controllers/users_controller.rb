71bb03f0be8440146fba194370791d4aa52507a4
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-13 08:32:09 +0000

    [Update] 4-25 ユーザ編集/更新機能におけるアクセス制限処理を1メソッドにまとめる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
-    user_id = params[:id].to_i
-    login_user_id = current_user.id
-    if(user_id != login_user_id)
-      redirect_to post_images_path
-    end
+    is_matching_login_user
     
     @user = User.find(params[:id])
   end
   
   def update
-    
-    user_id = params[:id].to_i
-    login_user_id = current_user.id
-    if(user_id != login_user_id)
-      redirect_to post_images_path
-    end
+    is_matching_login_user
     
     @user = User.find(params[:id])
     @user.update(user_params)
@@ -32,6 +22,14 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to post_images_path
+    end
   end
   
 end
